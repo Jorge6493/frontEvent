3ca39a8d1b555fa5a3c982aed18f5b5c50eb457e
Jorge6493 <jorge6493@gmail.com>
2020-12-03 17:37:36 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontEvent';
+  apikey = 'TVF5ZK7LOZXZTXCBAS3U';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {MatTableModule} from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+        BrowserAnimationsModule,
+        MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
